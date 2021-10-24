cc4074a71fc7eb85853af3f6ffe8764728cb71b1
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-10-24 15:31:49 -0600

    Add os_mon to applicaitons for live dashboard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Rostrum.MixProject do
   def application do
     [
       mod: {Rostrum.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
